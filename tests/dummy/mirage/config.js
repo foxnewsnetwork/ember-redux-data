6e314e668fc957555f9ace934be8432efa4f1804
Thomas Chen <tom.chen@am.sony.com>
2017-02-15 13:40:05 -0800

    added / to api to attempt to fix a testing issue

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -9,7 +9,7 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  this.namespace = 'api';    // make this `/api`, for example, if your API is namespaced
+  this.namespace = '/api';    // make this `/api`, for example, if your API is namespaced
   this.timing = 10;      // delay for each request, automatically set to 0 during testing
 
   /*
